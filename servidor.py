5aa8443967390df6f6e99ddd57b5f3bf65c8d3fa
gabryel-wsl <gabryel.dias@academico.ifpb.edu.br>
2023-05-30 20:37:20 -0300

    comentario do servidor

--- servidor.py
+++ servidor.py
@@ -19,7 +19,7 @@ def processarCliente(con, cliente):
         con.send(mensagem)
 
     print("Desconectando do cliente", cliente)
-    #mensagem do servidor: agradecemos a preferência
+    #mensagem do servidor: agradecemos a preferência teste
     con.close()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
