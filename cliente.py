de34441ffbdc43b290ed6073b07e9d844c5fe99f
islanpedro01 <113214808+islanpedro01@users.noreply.github.com>
2023-06-22 12:49:36 -0300

    Adição Dicionário, Adição Funções

--- cliente.py
+++ cliente.py
@@ -1,13 +1,13 @@
 import socket, sys
 from listaEncadeada import Lista, ListaException
-from menu import showMenu, Escolha_Cardapio, carrinho_pedidos
+from menu import *
+
 
 HOST = '127.0.0.1'
 PORTA = 41800
+mensagem = 0
 
-carrinho = Lista()
-confirmacao = ''
-mensagem = ''
+
 
 if len(sys.argv) > 1:
     HOST = sys.argv[1]
@@ -21,17 +21,17 @@ except ConnectionRefusedError as cre:
     print("A pizzaria se encontra fechada")
     sys.exit()
 
+
 while True:
-    try:
+    # try:
         menu = showMenu()
-        confirmacao = ""
-           
         if menu == "1":
-            while confirmacao != 'n':
+            # while confirmacao != 'n':
                 mensagem = Escolha_Cardapio(carrinho)
-                print("\nCarrinho: ", mensagem)
-                print("\nDeseja continuar comprando? (S/N)")
-                confirmacao = input('Opção: ').lower()
+                # print("\nCarrinho: ", mensagem)
+                # print(f'Total: {total:.2f}')
+                # print("\nDeseja continuar comprando? (S/N)")
+                # confirmacao = input('Opção: ').lower()
             
                     
         elif menu == "2":
@@ -39,19 +39,28 @@ while True:
             
 
         elif menu == "3":
-            sock.send(str.encode(mensagem))
-            mensagem = sock.recv(1024)
-            mensagem = mensagem.decode()
-            print('\nSeu pedido foi enviado com sucesso!')
-            input('\nAperte ENTER para voltar ao MENU...')
+            if mensagem != 0:
+                mensagem = contato(carrinho)
+                sock.send(str.encode(mensagem))
+                mensagem = sock.recv(1024)
+                mensagem = mensagem.decode()
+                print('\nSeu pedido foi enviado com sucesso!')
+                input('\nPressione ENTER para voltar ao MENU...')
+                mensagem = 0
+                total = 0
+            else:
+                limpaTerminal()
+                print("\nSeu carrinho está vazio! Adicione algo para fazer seu pedido!")   
+                input("\nPressione ENTER para ir ao cardápio...")
+                Escolha_Cardapio(carrinho) 
 
         elif menu == 'x':
             print("\nAgradecemos por sua preferência. Volte sempre!")
             break
 
-    except:
-        print("\n"+"Você saiu!")
-        break  
+    # except:
+            print("\n"+"Você saiu!")
+            break  
     
     #if not mensagem:
         #break
--- menu.py
+++ menu.py
@@ -1,9 +1,14 @@
 import os
 from listaEncadeada import Lista, ValorInexistenteException
+from cardapio import cardapio
+
+carrinho = Lista()
+total = 0
+
 
 def showMenu():
+    limpaTerminal()
     escolha = ''
-    os.system('cls')
     print("===Menu===")
     print("1 - Mostrar cardápio")
     print("2 - Abrir carrinho")
@@ -14,43 +19,69 @@ def showMenu():
 
 
 def Escolha_Cardapio(lista):
-    escolha = ''
-    os.system('cls')
-    print("===Cardápio===")
-    print("1 - Pepperoni")
-    print("2 - Frango com catupiry")
-    print("3 - Calabresa")
-    print("4 - Quatro queijos")
-    print("5 - À moda da casa")
-    escolha = input("Escolha uma opção: ").lower()
-    if escolha == "1":
-        lista.inserir(1, "Pepperonni")
-    elif escolha == "2":
-        lista.inserir(1, "Frango com catupiry")
-    elif escolha == "3":
-        lista.inserir(1, "Calabresa")
-    elif escolha == "4":
-        lista.inserir(1, "Quatro queijos")
-    elif escolha == "5":
-        lista.inserir(1, "À moda da casa")
+    confirmacao = ''
+    while True:
+        limpaTerminal()
+        escolha = ''
+        global total
+        print("===CARDÁPIO===\n")
+        for item in cardapio:
+            print(f'{item} - {cardapio[item][0]}: R$ {cardapio[item][1]:.2f}')
+        escolha = input("\nEscolha uma opção: ").lower()
+        if escolha in cardapio.keys():
+            lista.inserir(1, cardapio[escolha][0])
+            total += cardapio[escolha][1]
+        print("\nCarrinho: ", lista)
+        print(f'Total: {total:.2f}')
+        print("\nDeseja continuar comprando? (S/N)")
+        confirmacao = input('Opção: ').lower()    
+        if confirmacao == 'n':
+            return lista.__str__()
+    
+
+def pagamento(lista):
+    global total
+    limpaTerminal()
+    print("Para finalizar, preencha os campos abaixo:")
+    nome = input("Nome: ")
+    telefone = input("Telefone: ")
+    print("\nForma de pagamento:")
+    pagamento = input("1 - Cartão (pagamento na entrega) 2 - Dinheiro")
+    lista.inserir(1, nome)
+    lista.inserir(2, telefone)
+    if pagamento == '2':
+        print("Informe o valor em cédulas a ser pago para providenciarmos o troco: ")
+        valor = input("")
+        print(f'Seu troco será de R$ {valor-total:.2f}')
     return lista.__str__()
 
 def carrinho_pedidos(lista):
-    os.system('cls')
+    limpaTerminal()
+    global total
     print("===Carrinho===\n")
     print(lista)
+    print(f'Total: {total:.2f}')
     print("\n1 - Remover item")
     print("2 - Adicionar item")
+    print("3 - Voltar")
     
-    escolha = input ("Escolha uma opção: ").lower()
+    escolha = input ("\nEscolha uma opção: ").lower()
     if escolha == '1':
         item = input('Insira o sabor: ')
         if lista.busca(item) :
             lista.remover(lista.busca(item))
+            #total -= cardapio (SUBTRAIR DO VALOR TOTAL QUANDO REMOVER UM ITEM!)
             print (lista)
             carrinho_pedidos(lista)
     elif escolha == '2':
+        Escolha_Cardapio(lista)
+        return
+    else:
         return
         # except:
         #     raise ValorInexistenteException (f'0 sabor {item} não está no seu carrinho')
 # TEMP MENU, TEMP CARDAPIO, TEMP CARRINHO
+
+def limpaTerminal():
+    os.system('cls')
+    os.system('clear')
